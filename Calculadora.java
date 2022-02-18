a47d0945b6640ae6c1f7e33d4a2d2313fbca9510
unknown <aluno@L4M08.IPIRANGA.FATEC>
2022-02-18 17:58:17 -0300

    Implementação e Testes dos metodos somar e subtrair.

---
+++ Calculadora.java
@@ -0,0 +1,15 @@
+public class Calculadora{
+    private int result;
+
+    public void setResult(int result) {
+        this.result = result;
+    }
+
+    public int soma(int num1, int num2){
+      return result = num1 + num2;
+    }
+
+    public int subtrair(int num1, int num2){
+      return result = num1 - num2;
+    }
+}
---
+++ TesteCalculadora.java
@@ -0,0 +1,9 @@
+public class TesteCalculadora {
+    public static void main(String[] args) {
+        Calculadora calc = new Calculadora();
+
+        System.out.println (calc.soma(5,5));
+        System.out.println (calc.subtrair(5,5));
+    }
+  
+}
